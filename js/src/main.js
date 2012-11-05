d3e202656de17822d0439e27c5d66b3dae5e2aa3
Thomas Mullaly <tmullaly88@gmail.com>
2012-11-05 11:38:04 -0500

    Added jQuery shim

--- js/src/main.js
+++ js/src/main.js
@@ -1,3 +1,14 @@
+require.config({
+    shim: {
+        'jquery': {
+            exports: '$'
+        }
+    },
+    paths: {
+        'jquery': '../lib/jquery-1.8.2'
+    }
+});
+
 require(['app'], function(app) {
     "use strict";
 
